bac466af1e3335cb771c89f12d7ab054c63bb172
Kate_Nguyen <Kate_nguyen>
2026-04-23 11:34:42 -0700

    get method

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,6 +1,12 @@
 public class DynamicStringList implements StringList {
+    
     private String[] array;
     private int size;
+
+    public DynamicStringList() {
+        array = new String[10];
+        size = 0;
+    }
     /**
    * Retrieves the string at the specified index in the list.
    *
@@ -8,11 +14,12 @@ public class DynamicStringList implements StringList {
    * @return the string at the specified index.
    * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or index >= size()).
    */
-  public String get(int index) {
-    if (index < 0 || index >= size){
-        throw IndexOutOfBoundsException("Index" + index + "Size" + size );
-    }
-    return string;
+  public String get(int index) 
+  {
+    if (index < 0 || index >= size())
+        {
+        throw new IndexOutOfBoundsException("Index" + index + "Size" + size );
+        }
+    return array[index];
   }
-
 }
